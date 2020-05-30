1dc4fc3b4331b8c93f150317f8c3b6981714854d
Dalton Russell Cole <dalton.cole1@gmail.com>
2020-05-30 16:12:24 -0500

    Inital commit

---
+++ run.sh
@@ -0,0 +1,24 @@
+# /bin/bash
+
+### BASH RC ###
+cp ./.bashrc ~/.bashrc
+
+### TMUX ###
+cp ./.tmux.conf ~/.tmux.conf
+
+### VIM ###
+# Ultimate Vim rc install
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+# Move vimrc to home directory
+cp ./.vimrc ~/.vimrc
+
+# Install vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qal
+
+# YouCompleMe
+apt install build-essential cmake vim python3-dev # Install cmake, vim, python
+cd ~/.vim/bundle/YouCompleteMe
+python3 install.py --all # Will probably fail with some languages
